6dc6bd0c22b6a8a6f775ca9852ef93d108f6f866
kawatri <herinirina.data02@gmail.com>
2025-07-04 20:30:21 +0300

    update project

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,23 +1,27 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
-
-MYSQL_USER_PASSWORD="$(<"$MYSQL_PASSWORD_FILE")"
-
-USER_PASSWORD="$(<"$USER_PASSWORD_FILE")"
-ADMIN_PASSWORD="$(<"$ADMIN_PASSWORD_FILE")"
 
 if [ ! -f /var/www/hrazafia.42.fr/wp-config.php ]; then
   wp core download --allow-root
 
-  wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" \
-    --dbpass="$MYSQL_USER_PASSWORD" --dbhost="$MYSQL_HOST" --allow-root
+  wp config create --dbname="hrazafiadb" \
+                   --dbuser="hrazafia" \
+                   --dbpass="user1234" \
+                   --dbhost="mariadb" \
+                   --allow-root
 
-  wp core install --url="$DOMAIN_NAME" --title="$WEBSITE_TITLE" \
-    --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASSWORD" \
-    --admin_email="$ADMIN_MAIL" --skip-email --allow-root
+  wp core install --url="hrazafia.42.fr" \
+                  --title="Inception" \
+                  --admin_user="superuser" \
+                  --admin_password="user1234" \
+                  --admin_email="superuser@gmail.com" \
+                  --skip-email \
+                  --allow-root
 
-  wp user create $USER_NAME $USER_MAIL --user_pass=$USER_PASSWORD
-    --role=author --allow-root
+  wp user create 		simpleuser simpleuser@gmail.com \
+						--user_pass=user1234 \
+						--role=author \
+						--allow-root
 fi
 
 exec "$@"
