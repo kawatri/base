13763c85fb6055c6611dea5baa4fe89dfe90e919
kawatri <herinirina.data02@gmail.com>
2025-07-04 12:00:26 +0300

    update project

--- srcs/up.sh
+++ srcs/up.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker compose up -d --build
-mkdir -p /home/hrazafia/data/mariadb \
+mkdir -p /home/hrazafia/data/db \
 	/home/hrazafia/data/www/hrazafia.42.fr \
 	/home/hrazafia/data/ssl/hrazafia.42.fr \
 	/home/hrazafia/data/logs
